f2d28783cf3f73202a565de2200d48226b9a722a
venkatchand <venkatchand@users.noreply.github.com>
2021-01-28 12:26:09 +0530

    Update frontend stagename config

--- www/src/config.js
+++ www/src/config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "6pl9urah3t0hncm6pe1lh8udl1",     // CognitoClientID
-  "api_base_url": "https://aowfkzwb83.execute-api.eu-west-2.amazonaws.com/{StageNameParam}", // TodoFunctionApi
+  "api_base_url": "https://aowfkzwb83.execute-api.eu-west-2.amazonaws.com/prod", // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-eg-lambda.auth.eu-west-2.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "https://master.d1ao4gkla1yjz2.amplifyapp.com"                                      // AmplifyURL
 };
